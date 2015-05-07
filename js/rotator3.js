f187dce5eddaa61d378c509586c9a9cf4dced93e
Timo <timo.mulder@student.uva.nl>
2015-05-07 22:01:15 +0200

    projecten en fotos toegevoegd

---
+++ js/rotator3.js
@@ -0,0 +1,27 @@
+var images3 = new Array ('./img/portfolio/4.jpg', './img/portfolio/4-1.jpg');
+var index3 = 1;
+ 
+function rotateImage3()
+{
+  $('#project4').fadeOut('fast', function()
+  {
+    $(this).attr('src', images3[index3]);
+ 
+    $(this).fadeIn('fast', function()
+    {
+      if (index3 == images3.length-1)
+      {
+        index3 = 0;
+      }
+      else
+      {
+        index3++;
+      }
+    });
+  });
+}
+ 
+$(document).ready(function()
+{
+  setInterval (rotateImage3, 5000);
+});
---
+++ js/rotator4.js
@@ -0,0 +1,27 @@
+var images4 = new Array ('./img/portfolio/6.jpg', './img/portfolio/6-1.jpg');
+var index4 = 1;
+ 
+function rotateImage4()
+{
+  $('#project6').fadeOut('fast', function()
+  {
+    $(this).attr('src', images4[index4]);
+ 
+    $(this).fadeIn('fast', function()
+    {
+      if (index4 == images4.length-1)
+      {
+        index4 = 0;
+      }
+      else
+      {
+        index4++;
+      }
+    });
+  });
+}
+ 
+$(document).ready(function()
+{
+  setInterval (rotateImage4, 5000);
+});
